cbb5d3f53da37e8b4ad1d90e8014c300f4e6dd99
AzawuntuBlatena <azabla1227@gmail.com>
2024-08-15 16:27:52 +0300

    here is yours

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,29 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
-import Grid from '../components/Grid'
+import Grid from "../components/Grid";
 import RecentProjects from "@/components/RecentProjects";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Approach from "@/components/Approach";
 
 export default function Home() {
   return (
-   <main className="relative bg-black-100 flex justify-center items-center
-    overflow-hidden mx-auto sm:px-10 px-5">
-    <div className="max-w-7xl w-full">
-      <FloatingNav navItems={navItems} />
-      <Hero />
-      <Grid />
-      <RecentProjects/>
-      <Clients />
-
-    </div>
-   </main>
+    <main
+      className="relative bg-black-100 flex justify-center items-center
+     mx-auto sm:px-10 px-5 overflow-clip"
+    >
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
   );
 }
